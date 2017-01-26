8fa1aa07d4eb15dab127e288c2077e514d08db3b
Nicholas Blott <blottn@tcd.ie>
2017-01-26 10:39:29 +0000

    project finished, unsuccessful unfortunately

--- src/org/nblott/TestClass.java
+++ src/org/nblott/TestClass.java
@@ -1,10 +1,11 @@
 package org.nblott;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestClass {
 
-	@Test
+	@BeforeClass
 	public void anotherTest() {
 		System.out.println("Ran the other tests in test class TestClass");
 	}
